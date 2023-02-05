47156f1826720d7202f81b8d86f8c3e9e121e466
David Puente <davidpuent@gmail.com>
2023-02-06 00:28:31 +0100

    feat(component): :sparkles: open modal with issue body on click on issue

--- src/components/IssuesList.tsx
+++ src/components/IssuesList.tsx
@@ -1,16 +1,17 @@
 import type { Issue } from '../interfaces/GitHub'
 import { IssueListItem } from './IssueListItem'
 
-export function IssuesList ({ issues, headerText }: { issues: Issue[]; headerText?: string }) {
+export function IssuesList ({ issues, headerText, openModal }: { issues: Issue[]; headerText?: string, openModal: (issue: Issue) => void }) {
 	return (
 		<>
-			{headerText && <h2 className='text-xl'>{headerText}</h2>}
+			{headerText && <h2 className='text-3xl font-bold'>{headerText}</h2>}
 			{issues.length > 0 && (
 				<ul>
 					{issues.map((issue) => (
 						<li
 							key={issue.id}
-							className='grid grid-cols-7 gap-4 p-2 my-2 justify-evenly items-center border border-gray-600 rounded-lg bg-gray-800'
+							className='grid grid-cols-7 gap-4 p-2 my-2 justify-evenly items-center border border-gray-600 rounded-lg bg-gray-800 hover:bg-gray-700 cursor-pointer'
+							onClick={() => openModal(issue)}
 						>
 							<IssueListItem issue={issue} />
 						</li>
