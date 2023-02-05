08b3848dad6dc3263aa4643ade85e3a2888389b2
David Puente <davidpuent@gmail.com>
2023-02-06 00:27:02 +0100

    feat(component): :sparkles: remove link from issue list item

--- src/components/IssueListItem.tsx
+++ src/components/IssueListItem.tsx
@@ -1,5 +1,4 @@
 import type { Issue } from '../interfaces/GitHub'
-import { Link } from './Link'
 
 export function IssueListItem ({ issue }: { issue: Issue }) {
 	return (
@@ -13,13 +12,9 @@ export function IssueListItem ({ issue }: { issue: Issue }) {
 			>
 				{issue.labels.at(0)?.name ?? 'NO LABEL'}
 			</span>
-			<Link
-				customClass='col-span-4 hover:text-gray-200'
-				text={issue.title}
-				title={issue.body}
-				href={issue.html_url}
-				external={true}
-			/>
+			<span className='col-span-4 hover:text-gray-200'>
+				{issue.title}
+			</span>
 			{issue.prediction && (
 				<span
 					className='w-fit rounded-lg p-2 text-gray-900 font-black ml-auto relative col-span-2 min-w-[7em] text-center max-w-[12em] overflow-hidden overflow-ellipsis'
