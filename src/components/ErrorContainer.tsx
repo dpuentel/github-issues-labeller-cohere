c05973e0f9381e81894572b8a543a9b4e438ebb9
David Puente <davidpuent@gmail.com>
2023-02-25 20:20:24 +0100

    refactor(component): :sparkles: use a context in error container

--- src/components/ErrorContainer.tsx
+++ src/components/ErrorContainer.tsx
@@ -1,7 +1,23 @@
-export function ErrorContainer ({ error }: { error: Error | null }) {
-	return <section className='bg-red-800 rounded-lg p-4 ease-in-out duration-500'
-		style={{
-			opacity: error ? 1 : 0
-		}}
-	>{error?.message}</section>
+import { useContext } from 'react'
+import { IssuesContext } from '../context/issues'
+
+export function ErrorContainer () {
+	const context = useContext(IssuesContext)
+
+	if (!context) {
+		throw new Error('ErrorContainer must be used within a IssuesProvider')
+	}
+
+	const { issuesError: error } = context
+
+	return (
+		<section
+			className='bg-red-800 rounded-lg p-4 ease-in-out duration-500'
+			style={{
+				opacity: error ? 1 : 0
+			}}
+		>
+			{error?.message}
+		</section>
+	)
 }
