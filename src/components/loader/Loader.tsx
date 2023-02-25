4d29b43a0e9d55c108312136ec2cf23edd9a9b8c
David Puente <davidpuent@gmail.com>
2023-02-25 20:21:33 +0100

    refactor(component): :sparkles: use a context in loader component

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,10 +1,23 @@
+import { useContext } from 'react'
+import { IssuesContext } from '../../context/issues'
 import './Loader.css'
-export function Loader ({ isEnabled }: {isEnabled: boolean}) {
+
+export function Loader () {
+	const context = useContext(IssuesContext)
+
+	if (!context) {
+		throw new Error('Loader must be used within a IssuesProvider')
+	}
+
+	const { isLoading } = context
 	return (
 		<div className='loader-container'>
-			<span className='loader ease-in-out duration-500' style={{
-				opacity: isEnabled ? 1 : 0
-			}}></span>
+			<span
+				className='loader ease-in-out duration-500'
+				style={{
+					opacity: isLoading ? 1 : 0
+				}}
+			></span>
 		</div>
 	)
 }
